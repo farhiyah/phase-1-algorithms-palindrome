a1c06d96b2218179799d24e833a03bf546dcec82
farhiya dahir <farhiya.dahir@student.moringaschool.com>
2022-09-09 12:12:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const str = word.split("");
+  const strReverse = str.reverse().join("");
+  if (strReverse === word) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(isPalindrome("ab"));
+
 
 /* 
   Add your pseudocode here
 */
 
-/*
-  Add written explanation of your solution here
-*/
+
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
